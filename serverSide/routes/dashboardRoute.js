017f14179bbb879d7d5793b4a82ec6482fc33535
Emma Ilakut <51436325+emmailakut256@users.noreply.github.com>
2023-01-17 14:13:02 +0300

    dashboard route

--- serverSide/routes/dashboardRoute.js
+++ serverSide/routes/dashboardRoute.js
@@ -3,8 +3,28 @@ const router = express.Router();
 
 const passport = require('passport');
 
+const { pool }= require('../config/dbConfig');
+
+const app = express();
+
+const initializePassport = require('../config/passportConfig');
+
+initializePassport(passport);  
+
+
+// Middleware to check if someone accessing the dashbaord is logged in
+function checkNotAuthenticated(req, res, next){
+    if(req.isAuthenticated()){
+      return next();
+    }
+     res.redirect('/login');
+}
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Writing a route
-router.get('/dashboard',(req,res)=>{
+router.get('/dashboard', checkNotAuthenticated,  (req,res)=>{
     // console.log('',req.session.user)
     res.render('dashboard',{user: req.user.name});
     
