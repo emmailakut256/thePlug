3dded72cc0e833261e05b27418cb5d7a03194456
Emma Ilakut <51436325+emmailakut256@users.noreply.github.com>
2023-01-17 14:13:35 +0300

    add login route

--- serverSide/routes/loginRoute.js
+++ serverSide/routes/loginRoute.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const passport = require('passport');
+const app = express();
+const { pool }= require('../config/dbConfig');  //This is for postgres db connection
+const flash = require('express-flash');
+
 
 const router = express.Router();
-const initializePassport = require('./config/passportConfig');
+const initializePassport = require('../config/passportConfig');
 
 initializePassport(passport);  
 
+app.use(flash());
+
+// Middleware to check if the person accessing login is authenticated
+// function checkAuthenticated(req, res, next){
+//     if(req.isAuthenticated()){
+//       return res.redirect('/dashboard');
+//     }
+//     next();
+// }
 
 router.get('/login', (req, res)=> {
     res.render('login');
@@ -20,8 +33,15 @@ router.post('/login', passport.authenticate('local', {
  } 
 );
 
-app.use(passport.initialize());
-app.use(passport.session());
+
+
+router.get('/logout', (req, res)=> {
+    
+    req.flash("success_msg","You have logged out")
+    res.redirect('/login');
+    
+});
+
 
     
 module.exports = router;
